1751407945991b7b7c97ea89954805f63a68e928
Oğuz Çamur <oguzcamurr@gmail.com>
2025-09-27 21:34:46 +0300

    Update uninstallr.rb

--- Formula/uninstallr.rb
+++ Formula/uninstallr.rb
@@ -1,9 +1,10 @@
 class Uninstallr < Formula
   desc "Modern lightweight app uninstaller for macOS"
   homepage "https://github.com/oguzcamurr/uninstallr"
-  url "https://github.com/oguzcamurr/uninstallr/releases/download/v0.1.0/uninstallr-v0.1.0.tar.gz"
-  sha256 "cfe73e2223f09f07af43244b7ffd733b530d45fe65db8b0c2286b7a538b0906f"
+  url "https://github.com/oguzcamurr/uninstallr/releases/download/v0.1.1/uninstallr-v0.1.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
+  version "0.1.1"
 
   def install
     bin.install "uninstallr.sh" => "uninstallr"
