cc0b0dd53690c92c4d98b100c213d8425e4e9b5a
Oğuz Çamur <oguzcamurr@gmail.com>
2025-09-27 21:36:22 +0300

    Update uninstallr.rb

--- Formula/uninstallr.rb
+++ Formula/uninstallr.rb
@@ -1,8 +1,8 @@
 class Uninstallr < Formula
   desc "Modern lightweight app uninstaller for macOS"
   homepage "https://github.com/oguzcamurr/uninstallr"
-  url "https://github.com/oguzcamurr/uninstallr/releases/download/v0.1.1/uninstallr-v0.1.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/oguzcamurr/uninstallr/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "6a8a0c259974d1777f8e94247192c7a8723b537bfa7fb08deacabd4260c4d16f"
   license "MIT"
   version "0.1.1"
 
@@ -14,5 +14,3 @@ class Uninstallr < Formula
     assert_match "AppRemove Pro", shell_output("#{bin}/uninstallr --dry-run 'NoSuchApp' 2>&1")
   end
 end
-
-
